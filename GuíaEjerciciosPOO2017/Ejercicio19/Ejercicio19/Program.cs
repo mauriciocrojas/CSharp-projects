544b3b0924488a0424a85f8ddc3f3cb8dd2f9889
Mauricio Cesar Rojas <mauriciocc.rojas@gmail.com>
2021-10-16 11:28:03 -0300

    GuiaDeEjercicios

---
+++ GuíaEjerciciosPOO2017/Ejercicio19/Ejercicio19/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Clases;
+namespace Ejercicio19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word1 = "Hola,";
+            string word2 = " Juan Carlos";
+            long num1 = 17;
+            long num2 = 33;
+
+            Sumador operacion1 = new Sumador(10);
+            Sumador operacion2 = new Sumador(10);
+            Console.WriteLine("Cantidad de sumas inicial de la operacion 1: " + operacion1.Mostrar());
+            Console.WriteLine("Cantidad de sumas inicial de la operacion 2: " + operacion2.Mostrar());
+
+            //Se agregaron dos sumas
+            string cadena = operacion1.Sumar(word1, word2);
+            long enteros = operacion2.Sumar(num1, num2);
+
+            Console.WriteLine("Agregamos dos sumas:\nCadena: {0}\nEnteros: {1}", cadena, enteros);
+            Console.ReadKey();
+
+
+            long probarSuma = operacion1 + operacion2;
+            bool probarPipe = operacion1 | operacion2;
+
+            string comparacion = probarPipe ? "Si" : "No";
+
+            Console.WriteLine("Cantidad de sumas final de la operacion 1: " + operacion1.Mostrar());
+            Console.WriteLine("Cantidad de sumas final de la operacion 2: " + operacion2.Mostrar());
+
+            Console.WriteLine("Cantidad de sumas: " + probarSuma + " Son iguales?: " + comparacion);
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ GuíaEjerciciosPOO2017/Ejercicio19/Sumador/Sumador.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Clases
+{
+    public class Sumador
+    {
+        //Atributos
+        private int cantidadSumas;
+
+        //Constructores
+        public Sumador() : this(0)
+        {
+
+        }
+
+        public Sumador (int cantidadSumas)
+        {
+            this.cantidadSumas = cantidadSumas;
+        }
+
+        //Metodos
+        
+        public long Sumar (long a, long b)
+        {
+            this.cantidadSumas++;
+            return a + b;
+        }
+
+        public string Sumar (string a, string b)
+        {
+            this.cantidadSumas++;
+            return a + b;
+
+        }
+
+        //Sobrecarga de operadores
+
+        public static explicit operator int (Sumador s)
+        {
+            return s.cantidadSumas;
+        }
+
+
+        public static bool operator | (Sumador s1, Sumador s2)
+        {
+            return s1.cantidadSumas.Equals(s2.cantidadSumas);
+        }
+
+        public static long operator +(Sumador s1, Sumador s2)
+        {
+            return (long) (s1.cantidadSumas + s2.cantidadSumas);
+        }
+
+        
+        public int Mostrar()
+        {
+            return this.cantidadSumas;
+        }
+
+
+    }
+}
